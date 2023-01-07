ac254612024c806fdfe6f1b489cd24f745191e06
zephsinx <zephsinx@gmail.com>
2023-01-07 14:30:33 -0800

    Add YouTube chat support

--- README.md
+++ README.md
@@ -4,85 +4,76 @@
 ![GitHub contributors](https://img.shields.io/github/contributors/zephsinx/ComfyJazz)
 ![Twitch channel](https://img.shields.io/twitch/status/zephsinx?style=social)
 
-ComfyJazz is a tool to play easy-listening, comfy, computer-generated Jazz music, with built-in Twitch chat integration. Notes are played along randomly and for each chat message on your channel.
+ComfyJazz is a tool to play easy-listening, comfy, computer-generated Jazz music, with built-in Twitch and YouTube chat
+integration. Notes are played along randomly and for each chat message on your channel.
 
-### Table of Contents
+## Table of Contents
+
 - [Credits](#credits)
 - [Prerequisites](#prerequisites)
 - [Installing ComfyJazz](#installing-comfyjazz)
-- [Using ComfyJazz (simple)](#using-comfyjazz-simple)
-- [Using ComfyJazz (self-hosted)](#using-comfyjazz-self-hosted)
+- [Using ComfyJazz](#using-comfyjazz)
+    - [Twitch](#twitch)
+    - [YouTube](#youtube)
 
 ## Credits
 
-All credits to the original project go to [@Instafluff](https://github.com/instafluff) and all participants in the original project. Instafluff can also be found on Twitch at [ttv/Instafluff](https://twitch.tv/instafluff)
+All credits to the original project go to [@Instafluff](https://github.com/instafluff) and all participants in the
+original project. Instafluff can also be found on Twitch at [ttv/Instafluff](https://twitch.tv/instafluff)
 
 ## Prerequisites
 
-> NOTE: If you do not plan to run ComfyJazz locally and simply want to use a hosted version, skip to the [Using ComfyJazz (simple)](#using-comfyjazz-simple) section of the README.
-
 Before you begin, ensure you have met the following requirements:
-* Download and install [Node.js](https://nodejs.org/en/download/) matching for your operating system.
-  * The LTS (Long-term Support) version is recommended for most users.
-* Download and extract the [latest ComfyJazz code](https://github.com/zephsinx/ComfyJazz/archive/refs/heads/main.zip).
+
+- Download and install [Node.js](https://nodejs.org/en/download/) matching for your operating system.
+    - The LTS (Long-term Support) version is recommended for most users.
 
 ## Installing ComfyJazz
 
-ComfyJazz requires no installation beyond that listed in the [Prerequisites](#prerequisites) section.
+1. Navigate to the ComfyJazz folder.
+2. Run `npm install` to install required dependencies.
 
-## Using ComfyJazz (simple)
+## Using ComfyJazz
 
-The simplest way to set up ComfyJazz is to use the version hosted by Instafluff. This allows you to use ComfyJazz in your stream with no additional setup beyond configuring a Browser Source for your stream.
+### Twitch
 
-### Steps
-1. Add a new Browser Source to your broadcasting software (e.g. [OBS](https://obsproject.com/kb/browser-source)).
-2. In the URL field, enter `https://www.instafluff.tv/ComfyJazz?channel=yourchannel`.
-3. Replace `yourchannel` with your Twitch username.
-4. Enjoy!
-
-Example:
-```
-https://www.instafluff.tv/ComfyJazz?channel=zephsinx
-```
-
-## Using ComfyJazz (self-hosted)
-
-Another setup option is to host the application yourself. This provides multiple benefits such as:
-* Customizability, letting you add your own instruments other changes.
-* Better reliability and security due to not depending on someone else's hosted application.
-* Continued use of ComfyJazz in the case that `instafluff.tv/ComfyJazz` is not accessible.
-
-### Steps (Windows)
-1. Open a Windows command prompt by going to the start menu and typing `cmd`, and opening the `Command Prompt` application.
-2. Navigate to the root folder of the downloaded ComfyJazz code. (where the `index.js` is found)
-   1. `Method 1`: Take the folder path and type `cd <folder path>` in the command prompt
-      1. e.g. `cd C:\Projects\Software\ComfyJazz`
-   2. `Method 2`: Type `cmd` into the folder URL bar and hit Enter.
-3. Type `node index.js` and hit Enter.
-4. You should see a message displayed like the following:
-   1. `WebWebWeb is running on 8901` where `8901` is the port the ComfyJazz is running on.
-5. Add a new Browser Source to your broadcasting software (e.g. [OBS](https://obsproject.com/kb/browser-source)).
-6. In the URL field, enter `http://localhost:<port>?channel=yourchannel`.
-7. Replace `<port>` with the port output when ComfyJazz was run, and `yourchannel` with your Twitch username.
-8. Enjoy!
+1. Run the application from the command line.
+    1. `node index.js`
+2. You should see a message like `Listening on 8901`
+    1. Where 8901 is the port that ComfyJazz is listening on.
+3. Add a Browser Source to your broadcasting software (e.g. [OBS](https://obsproject.com/kb/browser-source)).
+4. In the URL field, enter `http://localhost:8901?channel=yourchannel`
+    1. Replace `yourchannel` with your Twitch username.
+5. Enjoy!
 
 Example:
-```
+
+```url
 http://localhost:8901?channel=zephsinx
 ```
 
 Notes:
+
 1. Keep the command prompt window open, as closing it will stop ComfyJazz.
-   1. If you would like ComfyJazz to run as a Windows service and not have to keep a command prompt open, [NSSM](https://nssm.cc/download) can be used for this purpose.
+    1. If you would like ComfyJazz to run as a Windows service and not have to keep a command prompt
+       open, [NSSM](https://nssm.cc/download) can be used for this purpose.
 2. To stop the application, press `CTRL+C` in the command prompt window, or simply close the command prompt.
 
-## Contributing to ComfyJazz
-To contribute to ComfyJazz, follow these steps:
+### YouTube
 
-1. Fork this repository.
-2. Create a branch: `git checkout -b <branch_name>`.
-3. Make your changes and commit them: `git commit -m '<commit_message>'`
-4. Push to the original branch: `git push origin ComfyJazz/<location>`
-5. Create the pull request.
+ComfyJazz also supports YouTube live chat. However, the Live ID of the YouTube stream must be provided on server start.
+As the YouTube event listener run on the server-side, notes are triggered via websocket requests to the client-side.
 
-Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+> Note: The server must be started _after_ the YouTube stream has begun to have a valid stream Live ID. Future
+> improvements will attempt to remove this requirement.
+
+1. Run the application from the command line and provide your YouTube Live ID.
+    1. `node index.js -y YOUR_LIVE_ID`
+2. You should see the following messages:
+    1. `Attempting connection to YouTube chat with Live ID: YOUR_LIVE_ID`
+    2. `Listening on 8901`
+        1. Where 8901 is the port that ComfyJazz is listening on.
+3. Add a Browser Source to your broadcasting software (e.g. [OBS](https://obsproject.com/kb/browser-source)).
+4. In the URL field, enter `http://localhost:8901?youtube=true`
+    1. `yourchannel` is not needed but doesn't hurt anything if kept.
+5. Enjoy!
--- web/ComfyJazz.js
+++ web/ComfyJazz.js
@@ -1,237 +1,259 @@
 //dependent on Howler.js https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.0/howler.min.js
 
 const ComfyJazz = (options = {}) => {
-  const cj = { ...defaultOptions, ...options };
+    const cj = {...defaultOptions, ...options};
 
-  /////////////////////
-  //ComfyJazz Methods
+    /////////////////////
+    //ComfyJazz Methods
 
-  cj.setVolume = (vol) => {
-    cj.volume = vol;
+    cj.setVolume = (vol) => {
+        cj.volume = vol;
 
-    if( cj.backgroundSound ) {
-      cj.backgroundSound.volume(vol);
-    }
-      if( cj.lastSound ) {
-      cj.lastSound.volume(vol);
-    }
-
-  };
-
-  cj.mute = () => cj.setVolume(0);
-  cj.unmute = () => cj.setVolume(1);
-  cj.isMuted = () => cj.volume <= 0;
-  cj.start = () => startComfyJazz();
-
-  cj.playNoteProgression = playNoteProgression;
-  cj.playNote = playNoteRandomly;
-
-  /////////////////////
-
-  async function startComfyJazz() {
-    let startTime = performance.now();
-
-    //initial start of the background music
-    playBackgroundSound(`${cj.soundFolder}/${cj.backgroundLoopUrl}`, cj.volume, 1); //1.0594630943592953
-
-    //this will Automatically play a note and then call itself again after a delay
-    const AutomaticPlayNote = async () => {
-      let currentTime = (performance.now() - startTime) / 1000;
-
-      if (currentTime > cj.backgroundLoopDuration) {
-        startTime = performance.now();
-        currentTime = 0;
-
-        // play the background music again
-        playBackgroundSound(`${cj.soundFolder}/${cj.backgroundLoopUrl}`, cj.volume, 1);
-      }
-
-      for (let i = 0; i < scaleProgressions.length; i++) {
-        if (scaleProgressions[i].start <= currentTime && currentTime <= scaleProgressions[i].end) {
-          currentScaleProgression = i;
-          break;
+        if (cj.backgroundSound) {
+            cj.backgroundSound.volume(vol);
         }
-      }
-
-      //play a note 20% of the time
-      if (cj.playAutoNotes && Math.random() < cj.autoNotesChance) {
-        playNoteRandomly(0, 200);
-      }
-
-      //here's what will loop
-      setTimeout(AutomaticPlayNote, cj.autoNotesDelay);
+        if (cj.lastSound) {
+            cj.lastSound.volume(vol);
+        }
     };
 
-    //start the Automatic Note Player loop
-    AutomaticPlayNote();
-  }
+    cj.mute = () => cj.setVolume(0);
+    cj.unmute = () => cj.setVolume(1);
+    cj.isMuted = () => cj.volume <= 0;
+    cj.start = () => startComfyJazz();
 
-  //Play a note with possible random delay
-  async function playNoteRandomly(minRandom = 0, maxRandom = 200) {
-    setTimeout(async () => {
-      let sound = getNextNote();
-	  const instruments = cj.instrument.split( "," ).map( x => x.trim() );
-	  let instrument = instruments[ getRandomInt( instruments.length ) ];
-      await playSound(`${cj.soundFolder}/${instrument}/${sound.url}.ogg`, cj.volume, sound.playbackRate);
-    }, minRandom + Math.random() * maxRandom);
-  }
+    cj.playNoteProgression = playNoteProgression;
+    cj.playNote = playNoteRandomly;
 
-  //Play a progression of notes, with random delay spacing!
-  function playNoteProgression(numNotes) {
-    if (numNotes == null) {
-      numNotes = (Math.random() * 8) >> 0;
+    /////////////////////
+
+    async function startComfyJazz() {
+        let startTime = performance.now();
+
+        //initial start of the background music
+        playBackgroundSound(
+            `${cj.soundFolder}/${cj.backgroundLoopUrl}`,
+            cj.volume,
+            1
+        ); //1.0594630943592953
+
+        //this will Automatically play a note and then call itself again after a delay
+        const AutomaticPlayNote = async () => {
+            let currentTime = (performance.now() - startTime) / 1000;
+
+            if (currentTime > cj.backgroundLoopDuration) {
+                startTime = performance.now();
+                currentTime = 0;
+
+                // play the background music again
+                playBackgroundSound(
+                    `${cj.soundFolder}/${cj.backgroundLoopUrl}`,
+                    cj.volume,
+                    1
+                );
+            }
+
+            for (let i = 0; i < scaleProgressions.length; i++) {
+                if (
+                    scaleProgressions[i].start <= currentTime &&
+                    currentTime <= scaleProgressions[i].end
+                ) {
+                    currentScaleProgression = i;
+                    break;
+                }
+            }
+
+            //play a note 20% of the time
+            if (cj.playAutoNotes && Math.random() < cj.autoNotesChance) {
+                playNoteRandomly(0, 200);
+            }
+
+            //here's what will loop
+            setTimeout(AutomaticPlayNote, cj.autoNotesDelay);
+        };
+
+        //start the Automatic Note Player loop
+        AutomaticPlayNote();
     }
 
-    for (let i = 0; i < numNotes; i++) {
-      playNoteRandomly(100, 200 * i);
-    }
-  }
-
-  ////////////////////////////////
-  //The fancy music playing functions
-  ////////////////////////////////
-
-  function semitonesToPlaybackRate(t) {
-    var e = Math.pow(2, 1 / 12);
-    return Math.pow(e, t);
-  }
-
-  function shiftSource( tone, startRange, endRange ) {
-  	let a = startRange
-  	  , u = endRange
-  	  , c = new WeakMap;
-
-  	// var e = c.get( tone );
-  	// if( e ) return e;
-  	let n = a + Math.random() * ( u - a );
-  	// c.set( tone, n );
-  	return n;
-  }
-
-  function playBackgroundSound(url, volume = 1, rate = 1) {
-    return new Promise((resolve, reject) => {
-      let a = new Howl({
-        src: [url],
-        volume: volume,
-        onend: function () {
-          resolve();
-        },
-      });
-      a.rate(rate);
-      a.play();
-      cj.backgroundSound = a;
-    });
-  }
-
-  function playSound(url, volume = 1, rate = 1) {
-    return new Promise((resolve, reject) => {
-      let a = new Howl({
-        src: [url],
-        volume: volume,
-        onend: function () {
-          resolve();
-        },
-      });
-      a.rate(rate);
-      a.play();
-      a.fade( volume, 0.0, 1000 );//a.duration() * 500 );
-      cj.lastSound = a;
-    });
-  }
-
-  function getNextNote() {
-    if (performance.now() - lastNoteTime > 900 || this.noteCount > maxNotesPerPattern) {
-      changePattern();
-      noteCount = 0;
+    //Play a note with possible random delay
+    async function playNoteRandomly(minRandom = 0, maxRandom = 200) {
+        setTimeout(async () => {
+            let sound = getNextNote();
+            const instruments = cj.instrument.split(",").map((x) => x.trim());
+            let instrument = instruments[getRandomInt(instruments.length)];
+            await playSound(
+                `${cj.soundFolder}/${instrument}/${sound.url}.ogg`,
+                cj.volume,
+                sound.playbackRate
+            );
+        }, minRandom + Math.random() * maxRandom);
     }
 
-    let e = scaleProgressions[currentScaleProgression];
-    scale = e.scale;
-    let n = getNote(scale);
-    while (n === lastNoteNumber) {
-      n = getNote(scale);
+    // Play a progression of notes, with random delay spacing!
+    function playNoteProgression(numNotes) {
+        if (numNotes == null) {
+            numNotes = (Math.random() * 8) >> 0;
+        }
+
+        for (let i = 0; i < numNotes; i++) {
+            playNoteRandomly(100, 200 * i);
+        }
+        return numNotes;
     }
 
-    // console.log(currentScaleProgressions, pattern, scale);
+    ////////////////////////////////
+    // The fancy music playing functions
+    ////////////////////////////////
 
-    if (e.root !== lastRoot) {
-      n = scaleifyNote(n, e.targetNotes);
+    function semitonesToPlaybackRate(t) {
+        var e = Math.pow(2, 1 / 12);
+        return Math.pow(e, t);
     }
 
-    var a = n || 48,
-      s = null;
-    s = notes.filter((x) => x.metaData.startRange <= a && a <= x.metaData.endRange)[0];
-	// NOTE: OOPS THIS MIGHT BE THE WRONG SPOT FOR SHIFTSOURCE
-	// let shifted = shiftSource( s.metaData.root, s.metaData.startRange, s.metaData.endRange );
-    let c = a - s.metaData.root;
-    let playbackRate = semitonesToPlaybackRate(c);
-    // console.log("playback", c, playbackRate);
-    let playNote = s;
-    playNote.playbackRate = playbackRate;
+    function shiftSource(tone, startRange, endRange) {
+        let a = startRange,
+            u = endRange,
+            c = new WeakMap();
 
-    noteCount++;
-    lastNoteTime = performance.now();
-    lastNoteNumber = n;
-    lastRoot = e.root;
-    return playNote;
-  }
-
-  function getNote(scale) {
-    // scale.length || (scale = e.scalesToUse[Math.floor(Math.random() * e.scalesToUse.length)]),
-    if (pattern < 0) {
-      changePattern();
+        // var e = c.get( tone );
+        // if( e ) return e;
+        let n = a + Math.random() * (u - a);
+        // c.set( tone, n );
+        return n;
     }
-    let t = patterns[pattern][currentStep];
-    let n = t + scales[scale][t % 12];
-    let r = transpose + n;
-    currentStep = (currentStep + 1) % patterns[pattern].length;
-    return r;
-  }
 
-  function getRandomInt(number) {
-    return Math.floor(number * Math.random());
-  }
-
-  function changePattern() {
-    pattern = getRandomInt(patterns.length);
-    currentStep = 0;
-  }
-
-  function getClosestTarget(t, e) {
-    return t.reduce(function (t, n) {
-      return Math.abs(n - e) < Math.abs(t - e) ? n : t;
-    });
-  }
-
-  function scaleifyNote(t, e) {
-    var n = ((t % 12) + 5) % 12;
-    if (
-      void 0 ==
-      e.filter(function (t) {
-        return t === n;
-      })[0]
-    ) {
-      var r = getClosestTarget(e, t),
-        o = (t -= n - r);
-      t = o += scales[scale][((o % 12) + 5) % 12];
+    function playBackgroundSound(url, volume = 1, rate = 1) {
+        return new Promise((resolve, reject) => {
+            let a = new Howl({
+                src: [url],
+                volume: volume,
+                onend: function () {
+                    resolve();
+                },
+            });
+            a.rate(rate);
+            a.play();
+            cj.backgroundSound = a;
+        });
     }
-    return t;
-  }
 
-  function getNoteFromSemitone(tone) {
-    return notes.filter((x) => x.metaData.startRange <= tone && tone <= x.metaData.endRange)[0];
-  }
+    function playSound(url, volume = 1, rate = 1) {
+        return new Promise((resolve, reject) => {
+            let a = new Howl({
+                src: [url],
+                volume: volume,
+                onend: function () {
+                    resolve();
+                },
+            });
+            a.rate(rate);
+            a.play();
+            a.fade(volume, 0.0, 1000); //a.duration() * 500 );
+            cj.lastSound = a;
+        });
+    }
 
-  let currentScaleProgression = 0;
-  let root = 0;
-  let lastRoot = undefined;
-  let pattern = -1;
-  let scale = "custom";
-  let transpose = -5;
-  let currentStep = 0;
-  let lastNoteTime = 0;
-  let lastNoteNumber = 0;
-  let noteCount = 0;
+    function getNextNote() {
+        if (
+            performance.now() - lastNoteTime > 900 ||
+            this.noteCount > maxNotesPerPattern
+        ) {
+            changePattern();
+            noteCount = 0;
+        }
 
-  return cj;
+        let e = scaleProgressions[currentScaleProgression];
+        scale = e.scale;
+        let n = getNote(scale);
+        while (n === lastNoteNumber) {
+            n = getNote(scale);
+        }
+
+        // console.log(currentScaleProgressions, pattern, scale);
+
+        if (e.root !== lastRoot) {
+            n = scaleifyNote(n, e.targetNotes);
+        }
+
+        var a = n || 48;
+        var s = null;
+        s = notes.filter(
+            (x) => x.metaData.startRange <= a && a <= x.metaData.endRange
+        )[0];
+        // NOTE: OOPS THIS MIGHT BE THE WRONG SPOT FOR SHIFTSOURCE
+        // let shifted = shiftSource( s.metaData.root, s.metaData.startRange, s.metaData.endRange );
+        let c = a - s.metaData.root;
+        let playbackRate = semitonesToPlaybackRate(c);
+        // console.log("playback", c, playbackRate);
+        let playNote = s;
+        playNote.playbackRate = playbackRate;
+
+        noteCount++;
+        lastNoteTime = performance.now();
+        lastNoteNumber = n;
+        lastRoot = e.root;
+        return playNote;
+    }
+
+    function getNote(scale) {
+        // scale.length || (scale = e.scalesToUse[Math.floor(Math.random() * e.scalesToUse.length)]),
+        if (pattern < 0) {
+            changePattern();
+        }
+        let t = patterns[pattern][currentStep];
+        let n = t + scales[scale][t % 12];
+        let r = transpose + n;
+        currentStep = (currentStep + 1) % patterns[pattern].length;
+        return r;
+    }
+
+    function getRandomInt(number) {
+        return Math.floor(number * Math.random());
+    }
+
+    function changePattern() {
+        pattern = getRandomInt(patterns.length);
+        currentStep = 0;
+    }
+
+    function getClosestTarget(t, e) {
+        return t.reduce(function (t, n) {
+            return Math.abs(n - e) < Math.abs(t - e) ? n : t;
+        });
+    }
+
+    function scaleifyNote(t, e) {
+        var n = ((t % 12) + 5) % 12;
+        if (
+            void 0 ==
+            e.filter(function (t) {
+                return t === n;
+            })[0]
+        ) {
+            var r = getClosestTarget(e, t);
+            var o = (t -= n - r);
+            t = o += scales[scale][((o % 12) + 5) % 12];
+        }
+        return t;
+    }
+
+    function getNoteFromSemitone(tone) {
+        return notes.filter(
+            (x) => x.metaData.startRange <= tone && tone <= x.metaData.endRange
+        )[0];
+    }
+
+    let currentScaleProgression = 0;
+    let root = 0;
+    let lastRoot = undefined;
+    let pattern = -1;
+    let scale = "custom";
+    let transpose = -5;
+    let currentStep = 0;
+    let lastNoteTime = 0;
+    let lastNoteNumber = 0;
+    let noteCount = 0;
+
+    return cj;
 };
